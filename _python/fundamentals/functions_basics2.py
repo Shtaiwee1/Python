ce29135623899c0b7e06338c4ab9cf4104278d5c
Shtaiwee <shtaiweeomair@gmail.com>
2022-01-30 23:32:00 +0200

    functions_basics_py

---
+++ _python/fundamentals/functions_basics2.py
@@ -0,0 +1,58 @@
+
+# ******************************COUNTDOWN******************
+
+# newlist=[]
+# def count(a):
+#   while(a>-1):
+#     print(a)
+#     newlist.append(a)
+#     a-=1
+#   return newlist
+
+# print(count(100))
+
+# **********************Print and Return*************************
+
+# def example(a):
+#   print(a[0])
+#   return a[1]
+
+# example([2,3])
+# ***************************First Plus Length *****************************************
+
+# sum=0
+# def solution(a):
+#   sum=a[0]+a[len(a)-1]
+#   return sum
+
+# print(solution([1,2,3,4,5,7]))
+
+# ****************************Values Greater than Second *********************
+
+
+# newlist = []
+# def x (a):
+#   count=0
+#   for i in range(0 , len(a),1):
+#     if a[i]>2:
+#       newlist.append(a[i])
+#       count+=1
+#   if len(newlist)<2:
+#     return 'false'
+#   else:
+#     return 'the new list is',newlist,'the numbers count is', count
+
+# print(x([5,2,3,2,1,4]))
+
+# **********************This Length, That Value************************************
+
+
+# def example(a):
+#   new=[]
+#   x=a[len(a)-1]
+#   y=a[0]
+#   for i in range(a[0],0,-1):
+#     new.append(x)
+#   return new
+
+# print(example([6,2]))
