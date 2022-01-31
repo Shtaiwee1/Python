f661634a999ec2401376b113c6dfd9cc32e30e85
Shtaiwee <shtaiweeomair@gmail.com>
2022-01-31 15:53:16 +0200

    changes

--- _python/fundamentals/functions_intermediate1.py
+++ _python/fundamentals/functions_intermediate1.py
@@ -1,8 +1,9 @@
 import random
 def randInt(min=0   , max=100  ):
+  while(min < max and max > 0):
     num = random.random()*(max-min)+min
     return round(num)
-# print(randInt()) 	
-# print(randInt(max=50))
-# print(randInt(min=50)) 	
+print(randInt()) 	
+print(randInt(max=50))
+print(randInt(min=50)) 	
 print(randInt(min=50, max=500))    # should print a random integer between 50 and 500
