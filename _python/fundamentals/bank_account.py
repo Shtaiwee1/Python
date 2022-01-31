ecba53ee71fb141e0ea59d3b6bccfeabd343d502
Shtaiwee <shtaiweeomair@gmail.com>
2022-02-01 01:22:43 +0200

    bank_account

--- _python/fundamentals/bank_account.py
+++ _python/fundamentals/bank_account.py
@@ -1,18 +1,19 @@
 class BankAccount:
-		def __init__(self, int_rate, balance):
-			self.int_rate = 0.01
-			self.balance = 0
+		def __init__(self, int_rate=0.01, balance=0):
+			self.int_rate = int_rate
+			self.balance = balance
 
 
 		def deposit(self, amount):
 			self.balance+=amount
 			return self
+ 
 		def withdraw(self, amount):
-			while amount<self.balance:
+			if amount<self.balance:
 				self.balance-=amount
-    
-			print('"Insufficient funds: Charging a $5 fee"')
-			self.balance-=5
+			elif amount > self.balance:
+				print('"Insufficient funds: Charging a $5 fee"')
+				self.balance-=5
 			return self
  
 		def display_account_info(self):
@@ -28,6 +29,6 @@ class BankAccount:
 firstaccount=BankAccount(0.02,2000)
 secondaccount=BankAccount(0.03,4000)
 
-firstaccount.deposit(300).deposit(400).deposit(500).withdraw(500).yield_interest().display_account_info()
+# firstaccount.deposit(300).deposit(400).deposit(500).withdraw(5000).yield_interest().display_account_info()
 
 secondaccount.deposit(300).deposit(400).withdraw(500).withdraw(500).withdraw(500).withdraw(500).yield_interest().display_account_info()
