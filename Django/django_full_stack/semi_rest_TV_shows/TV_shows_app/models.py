b5862d6cfe8ac2b80d6d2bb9c4aa860f71fbf089
Shtaiwee <shtaiweeomair@gmail.com>
2022-02-16 01:51:49 +0200

    some changes

--- Django/django_full_stack/semi_rest_TV_shows/TV_shows_app/models.py
+++ Django/django_full_stack/semi_rest_TV_shows/TV_shows_app/models.py
@@ -1,23 +1,26 @@
 from django.db import models
 # import datetime 
-# from time import localtime , strftime
-import time
+from datetime import datetime
+
 
 
 
 class ShowManager(models.Manager):
     def basic_validator(self, postData):
-        time=time.struct_time(tm_year=2022, tm_mon=2, tm_mday=15)
         errors = {}
 # add keys and values to errors dictionary for each invalid field
         if len(postData['title']) < 2:
             errors["title"] = "Show title should be at least 2 characters"
+        title_validate_unique=Show.objects.filter(title=postData['title'])
+        if len(title_validate_unique)> 0:
+            errors['title_unique'] = "Show already exists!"
         if len(postData['network']) < 3:
             errors["network"] = "Show network should be at least 3 characters"
         if len(postData['desc']) < 10:
             errors["desc"] = "Show description should be at least 10 characters"
-        if postData['date'] > time :
-            errors["date"] = "The date cannot be in the future!"
+        releasedate = datetime.strptime(postData["date"], "%Y-%m-%d")
+        if releasedate > datetime.now():
+            errors["date"]="Release date should be in the past"
         return errors
     
         
